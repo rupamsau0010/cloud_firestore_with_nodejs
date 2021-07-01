401a19dc32503d8dd3720216f81ed7e1c65d09e1
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-07-01 18:43:05 +0530

    adding userdata to firebase cloudstorage using express API

--- configs/cloudFirestore.js
+++ configs/cloudFirestore.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 
 const serviceAccount = require("../firebase_adminsdk.json");
 
-const createUser = () => {
+const cloudFirestore = () => {
     ///////////////////
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount)
@@ -11,19 +11,22 @@ const createUser = () => {
     
     const db = admin.firestore();
     ///////////////////
-    db.collection("generaluser").add({
-        name: "Rupam Sau",
-        city: "Contai",
-        occupation: "Engineering"
-    })
-    .then((res) => {
-        console.log("Data Inserted");
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+    // db.collection("generaluser").add({
+    //     name: "Rupam Sau",
+    //     city: "Contai",
+    //     occupation: "Engineering"
+    // })
+    // .then((res) => {
+    //     console.log("Data Inserted");
+    // })
+    // .catch((err) => {
+    //     console.log(err);
+    // })
+    ////////////////////
+
+    return db;
 }
 
-module.exports = createUser;
+module.exports = cloudFirestore;
 
 
---
+++ controllers/operationController.js
@@ -0,0 +1,62 @@
+// Import Depandencies
+
+// Import Local Depandencies
+const cloudFirestore = require("../configs/cloudFirestore");
+
+const db = cloudFirestore();
+
+module.exports.createUser_post = async(req, res) => {
+    db.collection("generaluser").add({
+        name: "Narandra Modi",
+        countryVisited: 69,
+        countryList: ["Bangladesh", "Pakistan", "Nepal", "Bhutan", "China"],
+        career: [
+            {
+                post: "partyLeader",
+                experience: 10,
+                age: 30,
+                work: ["gundami", "marpit", "Danga"]
+            },
+            {
+                post: "Chief Minister",
+                experience: 20,
+                age: 40,
+                work: ["More gundami", "More marpit", "More Danga"]
+            }, 
+            {
+                post: "Prime Minister",
+                experience: 10,
+                age: 60,
+                work: ["Most gundami", "Most marpit", "Most Danga"]
+            }
+        ],
+        wishList: {
+            asPartyLeader: {
+                inCountry: "Churi",
+                outCountry: "Dakati",
+            },
+            asChiefMinister: {
+                inCountry: "More Churi",
+                outCountry: "More Dakati",
+            },
+            asPrimeMinister: {
+                inCountry: "Most Churi",
+                outCountry: "Most Dakati",
+            }
+        }
+    })
+    .then((result) => {
+        console.log("Successfully Inserted");
+        res.json({
+            status: "success",
+            payload: result
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json({
+            status: "failure",
+            payload: err
+        })
+    })
+}
